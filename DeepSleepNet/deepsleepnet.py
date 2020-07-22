5b94d90f8b4ee18478324d254b6acb1872d23d06
leesk212 <67637935+leesk212@users.noreply.github.com>
2020-07-22 10:00:40 +0900

    Update deepsleepnet.py

--- DeepSleepNet/deepsleepnet.py
+++ DeepSleepNet/deepsleepnet.py
@@ -5,10 +5,6 @@ from tensorflow.keras.layers import Input, Conv1D, Dense, Dropout, MaxPool1D, Ac
 from tensorflow.keras.layers import Reshape, LSTM, TimeDistributed, Bidirectional, BatchNormalization
 from tensorflow.keras.models import load_model
 
-#random 변수는 training되지 않음
-#dropout이 0.5? 뭐를 줄이는 것인지는 모름
-
-
 def featurenet():
     activation = tf.nn.relu
     # activation = tf.nn.leaky_relu
@@ -26,7 +22,7 @@ def featurenet():
         strides=6,kernel_regularizer=keras.regularizers.l2(0.001)) #strides: 이동 간격
                                                                    #0.001수치는 우리가 건드는 것이 아님.
                                                                    #결과를 보고 최적의 0.001을 만든다.
-    s = cnn0(input_signal)
+    s = cnn0(input_signal) #First CNN
     s = BatchNormalization()(s) 
     s = Activation(activation=activation)(s)
     # print("cnn0:",s.shape)
