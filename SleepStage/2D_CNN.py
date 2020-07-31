ce9e89c4ff3d714a18216645124a81d8bd494028
leesk212 <67637935+leesk212@users.noreply.github.com>
2020-07-31 13:54:12 +0900

    Update 2D_CNN.py

--- SleepStage/2D_CNN.py
+++ SleepStage/2D_CNN.py
@@ -1,4 +1,4 @@
-#######################Success to operate MNIST#####################
+#######################Change 2D_CNN MNIST to 3000 by 6 #####################
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -15,10 +15,10 @@ print('Python version : ', sys.version)
 print('TensorFlow version : ', tf.__version__)
 print('Keras version : ', keras.__version__)
 
-img_rows = 28
-img_cols = 28
+img_rows = 3000 
+img_cols = 6
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = #"NEED to change"# #keras.datasets.mnist.load_data()
 
 input_shape = (img_rows, img_cols, 1)
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
@@ -64,6 +64,73 @@ hist = model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1]
+
+#######################Success to operate MNIST#####################
+#import warnings
+#warnings.simplefilter(action='ignore', category=FutureWarning)
+
+#import sys
+#import tensorflow as tf
+#import keras
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, Flatten
+#from keras.layers.convolutional import Conv2D, MaxPooling2D
+#import numpy as np
+#np.random.seed(7)
+
+#print('Python version : ', sys.version)
+#print('TensorFlow version : ', tf.__version__)
+#print('Keras version : ', keras.__version__)
+
+#img_rows = 28 
+#img_cols = 28
+
+#(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+#input_shape = (img_rows, img_cols, 1)
+#x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+#x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+#x_train = x_train.astype('float32') / 255.
+#x_test = x_test.astype('float32') / 255.
+
+#print('x_train shape:', x_train.shape)
+#print(x_train.shape[0], 'train samples')
+#print(x_test.shape[0], 'test samples')
+
+#batch_size = 128
+#num_classes = 10
+#epochs = 12
+
+#model = Sequential()
+#model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), padding='same',
+#                 activation='relu',
+#                 input_shape=input_shape))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#model.add(Conv2D(64, (2, 2), activation='relu', padding='same'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.25))
+#model.add(Flatten())
+#model.add(Dense(1000, activation='relu'))
+#model.add(Dropout(0.5))
+#model.add(Dense(num_classes, activation='softmax'))
+#model.summary()
+
+
+#y_train = keras.utils.to_categorical(y_train, num_classes)
+#y_test = keras.utils.to_categorical(y_test, num_classes)
+
+#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#hist = model.fit(x_train, y_train,
+#                 batch_size=batch_size,
+#                 epochs=epochs,
+#                 verbose=1, 
+#                 validation_data=(x_test, y_test))
+
+
+#score = model.evaluate(x_test, y_test, verbose=0)
+#print('Test loss:', score[0])
+#print('Test accuracy:', score[1]
 
 
 ###########################Fail to operate###########################
